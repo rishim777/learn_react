7ea945088b8206f439fbf4c514fc4c20b6fb65f4
Rishi <rishim777>
2022-03-08 15:56:50 +0530

    Added dependecy for reacstrap and react-toastify

--- react/learn_react1/src/App.js
+++ react/learn_react1/src/App.js
@@ -1,15 +1,14 @@
-// React can return only one component so if you want to return multiple components then use
-// make one parent element such as div and inside that create various child components
-import Card from "./Components/Card"
+import {Button} from 'reactstrap'
+import { ToastContainer, toast } from 'react-toastify';
+
 function App() {
+  const btn_handler = ()=>{
+    toast("Button Is Clicked");
+  };
   return (
     <div>
-      <Card name ="Ram" title="Happy Birthday Ram"/>
-      <hr/>
-      <Card name ="Shyam" title="Happy Birthday Shyam"/>
-      <hr/>
-      <Card name ="Sita" title="Happy Birthday Sita"/>
-      <hr/>
+      <ToastContainer/>
+      <Button color='danger' onClick={btn_handler}> CLICK ME</Button>
     </div>
   );
 }
